aa67f1889fef5f6550574fa28c40127b93bc5e67
saritahrdz <saritanaomi31@gmail.com>
2026-08-19 18:15:20 -0500

    Add files via upload

---
+++ Mi Coleccion/collection-data.js
@@ -0,0 +1,47 @@
+window.collectionData = {
+    "vinyls":  [
+                   {
+                       "title":  "Ones",
+                       "creator":  "Selena",
+                       "year":  "2002",
+                       "image":  "https://www.plaidroomrecords.com/cdn/shop/files/602508411229.webp?v=1714435649\u0026width=1200https://www.plaidroomrecords.com/cdn/shop/files/602508411229.webp?v=1714435649\u0026width=1200",
+                       "color":  "Picture",
+                       "edition":  "",
+                       "format":  ""
+                   }
+               ],
+    "cds":  [
+                {
+                    "title":  "Koi No Yokan",
+                    "creator":  "Deftones",
+                    "year":  "2012",
+                    "image":  "https://i.scdn.co/image/ab67616d0000b27373652b7a0dc388dd1d044b69",
+                    "color":  "",
+                    "edition":  "",
+                    "format":  ""
+                },
+                {
+                    "title":  "Diamond Eyes",
+                    "creator":  "Deftones",
+                    "year":  "2010",
+                    "image":  "https://i.scdn.co/image/ab67616d0000b2733ed600911146712083a3cb19",
+                    "color":  "",
+                    "edition":  "",
+                    "format":  ""
+                }
+            ],
+    "movies":  [
+                   {
+                       "title":  "Mulholland Dr.",
+                       "creator":  "David Lynch",
+                       "year":  "2001",
+                       "image":  "https://s3.amazonaws.com/criterion-production/films/dfdd32ea78dc954d8cef1b54e34acbf0/sCfUtouYZPKwTEfBNRMEntUVk572AT_large.jpg",
+                       "color":  "",
+                       "edition":  "Criterion Collection",
+                       "format":  "Blu-Ray"
+                   }
+               ],
+    "books":  [
+
+              ]
+};
